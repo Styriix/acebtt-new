a016e0ff62665af091fc27a2195de565eecf2c42
Styrix <acenetvx@gmail.com>
2020-06-06 16:05:14 +0100

    init

---
+++ application/logs/log-2019-04-28.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-28 11:29:15 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'USERNAME'@'server.bdtask.com' (using password: YES) /home/phpcryptomarketb/public_html/nishue-main_v1.9/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-28 11:29:15 --> Unable to connect to the database
+ERROR - 2019-04-28 11:29:27 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'USERNAME'@'server.bdtask.com' (using password: YES) /home/phpcryptomarketb/public_html/nishue-main_v1.9/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-28 11:29:27 --> Unable to connect to the database
+ERROR - 2019-04-28 11:29:30 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'USERNAME'@'server.bdtask.com' (using password: YES) /home/phpcryptomarketb/public_html/nishue-main_v1.9/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-28 11:29:30 --> Unable to connect to the database
+ERROR - 2019-04-28 11:29:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'USERNAME'@'server.bdtask.com' (using password: YES) /home/phpcryptomarketb/public_html/nishue-main_v1.9/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-28 11:29:33 --> Unable to connect to the database
